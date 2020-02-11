b8914a2a67d98ac8a327b755e9f278ba5d4d1511
Javier Julio <jjfutbol@gmail.com>
2020-02-11 12:31:23 -0500

    Bump elixir version to ~> 1.6
    This matches what other major libraries use like Ecto and what Elixir documents as supported versions: https://github.com/elixir-lang/elixir/blob/master/lib/elixir/pages/Compatibility%20and%20Deprecations.md

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Binlist.Mixfile do
     [
       app: :binlist,
       version: "0.1.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       deps: deps(),
       package: package()
