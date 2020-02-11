eb75c156e05af72defc5fddcdaf617c3bd7e0960
Javier Julio <jjfutbol@gmail.com>
2020-02-11 12:14:33 -0500

    Bump poison to latest release
    binlist-elixir: (master) mix deps.update poison                                                                                         11:19:09
    Resolving Hex dependencies...
    Dependency resolution completed:
    Unchanged:
      certifi 2.5.1
      earmark 1.4.3
      ex_doc 0.21.3
      hackney 1.15.2
      httpoison 1.6.2
      idna 6.0.0
      makeup 1.0.0
      makeup_elixir 0.14.0
      metrics 1.0.1
      mimerl 1.2.0
      nimble_parsec 0.5.3
      parse_trans 3.3.0
      ssl_verify_fun 1.1.5
      unicode_util_compat 0.4.1
    Upgraded:
      poison 3.1.0 => 4.0.1 (major)
    * Updating poison (Hex package)

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Binlist.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 1.0"},
-      {:poison, "~> 3.1"},
+      {:poison, "~> 4.0"},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
