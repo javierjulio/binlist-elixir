6f5f2de199936027e70e0bcbd6d0aabad3456c02
Javier Julio <jjfutbol@gmail.com>
2020-02-11 11:22:33 -0500

    Bump httpoison to latest release
    binlist-elixir: (master) mix deps.update httpoison                                                                                      11:05:05
    Resolving Hex dependencies...
    Dependency resolution completed:
    Unchanged:
      earmark 1.4.3
      ex_doc 0.21.3
      makeup 1.0.0
      makeup_elixir 0.14.0
      metrics 1.0.1
      nimble_parsec 0.5.3
      poison 3.1.0
    Upgraded:
      certifi 2.0.0 => 2.5.1
      hackney 1.9.0 => 1.15.2
      httpoison 0.13.0 => 1.6.2 (major)
      idna 5.1.0 => 6.0.0 (major)
      mimerl 1.0.2 => 1.2.0
      ssl_verify_fun 1.1.1 => 1.1.5
      unicode_util_compat 0.3.1 => 0.4.1 (minor)
    New:
      parse_trans 3.3.0

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Binlist.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 0.13"},
+      {:httpoison, "~> 1.0"},
       {:poison, "~> 3.1"},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
